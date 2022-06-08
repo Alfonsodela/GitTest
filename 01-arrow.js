2b4ec292b8cb60d24ad13d67e594b2e145552a89
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-06-08 21:03:10 +0200

    add first commit

---
+++ 01-arrow.js
@@ -0,0 +1,41 @@
+// Iteración #1: Arrows
+//
+// Crea una arrow function que tenga dos parametros a y b y
+// que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre
+// por consola la suma de los dos parametros.
+
+// let myFunction = (a,b) => {
+// let sum = a+b;
+// return sum
+// }
+// let res = myFunction(10,5);
+// console.log(res);
+
+// 1.1 Ejecuta esta función sin pasar ningún parametro
+
+// let myFunction = (a,b) => {
+// let sum = a+b;
+// return sum
+// }
+// let res = myFunction();
+// console.log(res);
+
+// 1.2 Ejecuta esta función pasando un solo parametro
+
+// let myFunction = (a,b) => {
+// let sum = a+b;
+// return sum
+// }
+// let res = myFunction(10);
+// console.log(res);
+//
+
+// 1.3 Ejecuta esta función pasando dos parametros
+
+// const myFunction = (a, b) => {
+//   let sum = a + b;
+//   return sum;
+// };
+// 
+// let res = myFunction(10, 5);
+// console.log(res);
